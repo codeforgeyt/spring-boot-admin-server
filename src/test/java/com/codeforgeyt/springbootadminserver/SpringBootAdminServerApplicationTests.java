b31855d71f3225b728d1e057594ef9ed33a71f90
codeforgeyt <codeforgeyt@gmail.com>
2021-01-03 11:22:48 +0100

    Initial commit

---
+++ src/test/java/com/codeforgeyt/springbootadminserver/SpringBootAdminServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeforgeyt.springbootadminserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAdminServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
